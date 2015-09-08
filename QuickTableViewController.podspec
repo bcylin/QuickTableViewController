88bd57b701a4ceff5e6c717c5e178cbdeadec62a
Ben <b.cylin@gmail.com>
2015-09-08 22:09:46 +0800

    Rename the Source directory

--- QuickTableViewController.podspec
+++ QuickTableViewController.podspec
@@ -7,6 +7,6 @@ Pod::Spec.new do |s|
 
   s.platform      = :ios, "8.0"
   s.source        = { :git => "https://github.com/bcylin/QuickTableViewController.git", :tag => "v#{s.version}" }
-  s.source_files  = "QuickTableViewController/*.swift"
+  s.source_files  = "Source/*.swift"
   s.requires_arc  = true
 end
--- Source/QuickTableViewController.h
+++ Source/QuickTableViewController.h
@@ -33,5 +33,3 @@ FOUNDATION_EXPORT double QuickTableViewControllerVersionNumber;
 FOUNDATION_EXPORT const unsigned char QuickTableViewControllerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <QuickTableViewController/PublicHeader.h>
-
-
