6cf0f51808fc0d42dd3f89f310d9c399662b65d8
Ben <b.cylin@gmail.com>
2016-01-17 16:27:55 +0800

    Describe NavigationRow

--- QuickTableViewControllerTests/RowSpec.swift
+++ QuickTableViewControllerTests/RowSpec.swift
@@ -11,25 +11,6 @@ import QuickTableViewController
 
 class RowSpec: XCTestCase {
 
-  func testNavitationRowEquality() {
-    let image = UIImage()
-    let a = NavigationRow(title: "Same", subtitle: .BelowTitle("Same"), icon: Icon(image: image), action: nil)
-    let b = NavigationRow(title: "Same", subtitle: .BelowTitle("Same"), icon: Icon(image: image), action: nil)
-    XCTAssert(a == b)
-
-    let c = NavigationRow(title: "Different", subtitle: .BelowTitle("Same"), icon: Icon(image: image), action: nil)
-    XCTAssert(a != c)
-
-    let d = NavigationRow(title: "Same", subtitle: .BelowTitle("Different"), icon: Icon(image: image), action: nil)
-    XCTAssert(a != d)
-
-    let e = NavigationRow(title: "Same", subtitle: .BelowTitle("Same"), icon: Icon(image: UIImage()), action: nil)
-    XCTAssert(a != e)
-
-    let f = NavigationRow(title: "Same", subtitle: .BelowTitle("Same"), icon: Icon(image: image), action: { _ in })
-    XCTAssert(a == f)
-  }
-
   func testSwitchRowEquality() {
     let a = SwitchRow(title: "Same", switchValue: true, action: nil)
     let b = SwitchRow(title: "Same", switchValue: true, action: nil)
