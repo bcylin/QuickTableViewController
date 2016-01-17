2c0ce10d10bcf858dad515698790671036c0e686
Ben <b.cylin@gmail.com>
2016-01-17 17:44:09 +0800

    Describe SwitchRow

--- QuickTableViewControllerTests/RowSpec.swift
+++ QuickTableViewControllerTests/RowSpec.swift
@@ -11,21 +11,6 @@ import QuickTableViewController
 
 class RowSpec: XCTestCase {
 
-  func testSwitchRowEquality() {
-    let a = SwitchRow(title: "Same", switchValue: true, action: nil)
-    let b = SwitchRow(title: "Same", switchValue: true, action: nil)
-    XCTAssert(a == b)
-
-    let c = SwitchRow(title: "Same", switchValue: false, action: nil)
-    XCTAssert(a != c)
-
-    let d = SwitchRow(title: "Different", switchValue: true, action: nil)
-    XCTAssert(a != d)
-
-    let e = SwitchRow(title: "Same", switchValue: true, action: { _ in })
-    XCTAssert(a == e)
-  }
-
   func testTapActionRowEquality() {
     let a = TapActionRow(title: "Same", action: nil)
     let b = TapActionRow(title: "Same", action: nil)
