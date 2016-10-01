60b55405e4d3ab451bfc205866c849e15ae14e30
Ben <b.cylin@gmail.com>
2016-10-02 02:26:24 +0800

    Push gh-pages using GitHub API token

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -16,5 +16,14 @@ cp -rfv output/* .
 git --no-pager diff --stat
 git add .
 git commit -m "[CI] Update documentation at $(date +'%Y-%m-%d %H:%M:%S %z')"
-bundle exec ruby ../scripts/push-gh-pages.rb
+
+if [ "${TRAVIS_BRANCH}" = "feature/auto-generated-docs" ]; then
+  echo "Updating gh-pages"
+  git remote add upstream "https://${GH_PAGES_GITHUB_API_TOKEN}@github.com/bcylin/QuickTableViewController.git"
+  git push --quiet upstream HEAD:gh-pages
+  git remote remove upstream
+else
+  echo "Skip gh-pages updates on ${TRAVIS_BRANCH}"
+fi
+
 cd -
