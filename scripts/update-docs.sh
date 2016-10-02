ddbd8f9e18a9aac428c171cfefd25bf20cc68e53
Ben <b.cylin@gmail.com>
2016-10-02 15:41:01 +0800

    Skip gh-pages updates on branches other than develop

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -17,7 +17,7 @@ git --no-pager diff --stat
 git add .
 git commit -m "[CI] Update documentation at $(date +'%Y-%m-%d %H:%M:%S %z')"
 
-if [ "${TRAVIS_BRANCH}" = "feature/auto-generated-docs" ]; then
+if [ "${TRAVIS_BRANCH}" = "develop" ]; then
   echo "Updating gh-pages"
   git remote add upstream "https://${GH_PAGES_GITHUB_API_TOKEN}@github.com/bcylin/QuickTableViewController.git"
   git push --quiet upstream HEAD:gh-pages
