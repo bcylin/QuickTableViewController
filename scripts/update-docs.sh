21448931349cc1649057ed8ae32597a76cbc4c4a
Ben <b.cylin@gmail.com>
2018-04-20 11:13:43 +0800

    Update docs on master branch

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -2,7 +2,7 @@
 
 cd docs && pwd
 
-if [ "${TRAVIS_BRANCH}" = "develop" ] && [ -n "$DANGER_GITHUB_API_TOKEN" ]; then
+if [ "${TRAVIS_BRANCH}" = "master" ] && [ -n "$DANGER_GITHUB_API_TOKEN" ]; then
   echo "Updating gh-pages"
   git remote add upstream "https://${GH_PAGES_GITHUB_API_TOKEN}@github.com/bcylin/QuickTableViewController.git"
   git push --quiet upstream HEAD:gh-pages
