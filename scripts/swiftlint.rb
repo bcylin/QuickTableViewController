89b329678eec07297a57cb37a6aa77925e1b4a3c
Ben <b.cylin@gmail.com>
2019-05-29 01:12:31 +0100

    Reinstall SwiftLint

--- scripts/swiftlint.rb
+++ scripts/swiftlint.rb
@@ -1,10 +1,9 @@
 #!/usr/local/env ruby
 
-swiftlint_installed = system "which swiftlint >/dev/null"
 not_ci = !ENV["TRAVIS"] || ENV["TRAVIS"]&.empty?
 
-if not_ci and swiftlint_installed
-  system "swiftlint --config .swiftlint.yml"
+if not_ci
+  system "${PODS_ROOT}/SwiftLint/swiftlint --config .swiftlint.yml"
 else
   puts "Skip SwiftLint"
 end
